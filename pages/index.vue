067db03b89f5a3a9a77e35a2d33e16d77020b083
mahmudul <imdmahmudul237@gmail.com>
2024-01-19 23:40:48 +0600

    create landing page

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,85 @@
 <template>
-  <div>Home page</div>
+ <div class="bg-[#0F0E0A]  ">
+  <div class=" max-w-5xl mx-auto grid grid-cols-2 gap-8 py-12">
+    <div>
+      <h1 class="text-white  text-5xl leading-[60px] font-bold mr-4">Free AI Image Generator: Turn text to images in seconds</h1>
+      <p class="text-white text-base mt-4">Convert words into an image in mere seconds with the Picsart AI Image Generator. Type a detailed description and watch your vision transform into vibrant, vibrant piece of art. 
+</p>
+<div class="flex items-center gap-3 mt-6">
+  <input class=" text-base w-full py-2 px-2 rounded-lg" type="text" placeholder="Describe what you want to see" name="" id="" >
+<button class="bg-[#C209C1] rounded-lg py-2 px-6 font-medium text-base text-white">Generate</button>
+</div>
+<button class=" border border-[#C209C1] text-sm bg-transparent text-[#C209C1] rounded-[20px] py-1.5 px-6 font-bold mt-6">Prompt sample</button>
+    </div>
+    <div>
+      <img src="../static/hero-image.png" alt="">
+    </div>
+  </div>
+ </div>
+ <div class="bg-white py-12 h-full">
+      <div class="w-[90%] max-w-[700px] mx-auto text-center mb-20">
+        <h2 class="text-black text-5xl sm:text-[28px] font-bold  leading-9">
+          AI images in various styles
+        </h2>
+        <p class="text-[#666666] text-lg font-medium leading-8 mt-4">
+          Explore AI-generated images in diverse styles, unveiling a captivating collection of visuals crafted by AI.
+        </p>
+      </div>
+      <div class="w-[90%] max-w-6xl mx-auto">
+        <div class="grid grid-cols-3 gap-4 two-column-grid sm:grid-cols-1 text-center">
+         <div>
+          <img class="rounded-xl" src="../static/image.webp" alt="">
+         </div>
+         <div>
+          <img class="rounded-xl" src="../static/image2.webp" alt="">
+         </div>
+         <div>
+          <img class="rounded-xl" src="../static/image3.webp" alt="">
+         </div>
+        </div>
+        <div class="mx-auto text-center mt-10">
+          <nuxt-link to="/app" class="bg-[#C209C1] rounded-lg py-2 px-6 font-medium text-base text-white">Try text to image</nuxt-link>
+        </div> 
+      </div>
+    </div>
+
+ <div class="bg-white py-24">
+      <div class="w-[90%] max-w-5xl mx-auto">
+        <div class="flex items-center mb-16 gap-16 sm:gap-8 sm:flex-col-reverse">
+          <div class="w-1/2 sm:w-full">
+            <h3 class="text-3xl text-[#343434] font-semibold leading-normal mb-4 sm:text-xl sm:font-bold">
+            Introducing  AI Image Generator apps
+            </h3>
+            <p class="text-base text-[#666666] font-medium leading-8">
+             Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut illum, nobis ipsum quidem saepe perspiciatis esse quibusdam consequuntur autem commodi. Delectus quis voluptas velit assumenda soluta beatae incidunt accusamus voluptate!
+            </p>
+          </div>
+          <div class="w-1/2 sm:w-full bg-white py-6 rounded-xl">
+            <img  class="h-[300px] w-full rounded-xl" src="../static/feature.png" alt="">
+          </div>
+        </div>
+        <div class="flex items-center  gap-16 sm:gap-8 sm:flex-col-reverse">
+        
+          <div class="w-1/2  sm:w-full bg-white py-6 rounded-xl">
+            <img class="h-[300px] w-full rounded-xl" src="../static/feature2.png" alt="">
+          </div>
+          <div class="w-1/2 sm:w-full">
+            <h3 class="text-3xl text-[#343434] font-semibold leading-normal mb-4 sm:text-xl sm:font-bold">
+           Choose your image style with Text to Image
+            </h3>
+            <p class="text-base text-[#666666] font-medium leading-8">
+             Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut illum, nobis ipsum quidem saepe perspiciatis esse quibusdam consequuntur autem commodi. Delectus quis voluptas velit assumenda soluta beatae incidunt accusamus voluptate!
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
 </template>
 
-<script setup>
+<script>
+export default {
 
+}
 </script>
 
 <style>
