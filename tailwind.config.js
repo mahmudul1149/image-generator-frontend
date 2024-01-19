eb96f683c797f35178bdea618e9db1f29383897c
mahmudul <imdmahmudul237@gmail.com>
2024-01-19 23:42:08 +0600

    initilize project

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: [],
   theme: {
-    extend: {},
+    screens: {
+      sm: { max: "639px" },
+      md: { max: "767px" },
+      lg: { max: "1023px" },
+      xl: { max: "1279px" },
+    },
   },
   plugins: [],
 }
