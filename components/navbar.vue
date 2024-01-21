861bffaf09383d694480e7e9d833d01e8c8cb4a9
mahmudul <imdmahmudul237@gmail.com>
2024-01-21 16:53:22 +0600

    add navbar section

--- components/navbar.vue
+++ components/navbar.vue
@@ -2,42 +2,70 @@
     <div class="px-7 py-[.9rem] sm:px-5 bg-[#0F0E0A] border-b border-gray-800">
       <div class="max-w-6xl m-auto flex justify-between align-middle items-center">
         <div>
-          <nuxt-link to="/" class="nav-logo text-center items-center justify-center leading-3 flex text-[#73FFEC] font-bold text-lg">
-            <h1>Image Generate</h1>
+          <nuxt-link to="/" class="logo text-center items-center justify-center leading-3 flex   font-bold text-lg">
+            <h1>Image Generator</h1>
           </nuxt-link>
         </div>
         <div>
           <ul class="flex justify-between items-center">
             <li class="text-white text-opacity-80 mr-8 sm:mr-5 ">
                 
-                <nuxt-link class="font-medium text-white sm:text-base sm:text-[1.1rem] hover:text-[#A6E22E] py-4 transition"
+                <nuxt-link class="font-medium text-white sm:text-base sm:text-[1.1rem] hover:text-[#A6E22E]  transition"
                   to="/">
                   Home
                 </nuxt-link>
               </li>
-            <li class="text-white text-opacity-80 mr-8 sm:mr-5 ">
+            <li v-if="store.user" class="text-white text-opacity-80 mr-8 sm:mr-5 ">
                 
-              <nuxt-link class=" font-medium text-white sm:text-base hover:text-[#A6E22E] py-4 transition"
+              <nuxt-link  class=" font-medium text-white sm:text-base hover:text-[#A6E22E]  transition"
                 to="/app">
                 App
               </nuxt-link>
             </li>
-            <li class="text-white text-opacity-80 mr-8 sm:mr-5 ">
+            <li v-if="!store.user" class="text-white text-opacity-80 mr-8 sm:mr-5 ">
                 
-                <button class="border border-[#C209C1] text-white py-2 px-4 rounded-lg">Log in or sign up</button>
+                <button class="border border-[#c6abc6] text-white py-2 px-4 rounded-lg" @click="store.signinWithGoogle">Sign in</button>
+              </li>
+              <li v-else>
+                   <img @click.stop="showPopupProfile = !showPopupProfile" class="w-10 h-10 rounded-full border-2 border-[#C209C1] cursor-pointer" :src="store.user.photoURL" alt="">
               </li>
             
           </ul>
+          <div v-show="showPopupProfile" class=" absolute top-0 right-0 mt-16 mr-6">
+            <popup-profile></popup-profile>
+          </div>
+          
         </div>
       
       </div>
       
     </div>
   </template>
-  
+  <script setup>
+  import { useMainStore } from '../store/index';
+  const store = useMainStore();
+  const showPopupProfile = ref(false)
+</script>
   <style scoped>
   .link:active {
     border-bottom: 2px solid #9030ed;
   }
+ 
+
+
+
+.logo {
+  font-family: 'Ubuntu', sans-serif;
+  font-size: 35px;
+  font-weight: bold;
+  color: #121212;
+  text-align: center;
+  letter-spacing: 5px;
+  text-shadow: -1px 1px 0 #41ba45,
+          1px 1px 0 #c63d2b,
+         1px -1px 0 #42afac,
+        -1px -1px 0 #c6c23f;
+}
+
   </style>
   ``
